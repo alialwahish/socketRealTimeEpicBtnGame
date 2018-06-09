681e64ac7fed9799db70a8747c6cd348ed341d98
Ali Bayati <bayati.ali@icloud.com>
2018-06-09 14:39:12 -0500

    used io.emit

--- server.js
+++ server.js
@@ -11,19 +11,16 @@ io.on('connection',function(socket){
 
     socket.emit('greeting',{msg:"Greetings, from server Node"});
     socket.on('thankyou',function(data){
-    socket.broadcast.emit('curEpic',epic)
-    socket.emit('curEpic',epic)
+    io.emit('curEpic',epic)
 
     });
     socket.on('addEpic',function(){
         epic++;
-        socket.broadcast.emit('curEpic',epic)
-        socket.emit('curEpic',epic)
+        io.emit('curEpic',epic)
     })
     socket.on('restEpic',function(){
         epic=0;
-        socket.broadcast.emit('curEpic',epic)
-        socket.emit('curEpic',epic)
+        io.emit('curEpic',epic)
     })
 
 });
